c5df2873326bba293426128d570f008b1723279a
Robinson Espejo <robinson.espejo.1998@gmail.com>
2020-06-03 00:20:59 -0500

    proyect1

---
+++ js/script.js
@@ -0,0 +1,43 @@
+/*//var a=1;
+var a=false;
+var b=2;
+var c="3";
+var texto1="Hola";
+var texto2="adios";
+console.log(a+b);
+console.log(texto1 +" "+texto2);
+texto1 += " Juan";
+console.log(texto1+" "+texto2);
+texto1+=a;
+console.log(texto1);
+console.log(a+c);
+if(!(juibilado=true)){
+	
+}
+if(a == 13){
+	console.log("Verdadero");
+	console.log(a);
+}
+else{
+	console.log("Falso");
+	console.log(a);
+}*/
+
+var a="Ya";
+var b="entendió";
+var varnombre = document.getElementById("Nombre").value;
+document.getElementById("Texto").innerHTML=Nombre+" "+a+" "+b;
+
+document.getElementById("boton").onclick=function clic2(){
+	varnombre = document.getElementById("nombre").value;
+	document.getElementById("texto").innerHTML = varnombre+" "+a+" "+b;
+}};
+
+var i;
+
+for(i=){
+	
+}
+document.getElementById("producto").innerHTML= "<option value='carro'>carro</option>\
+												<option value='moto'>moto</option>\
+												<option value='bici'>bici</option>";
