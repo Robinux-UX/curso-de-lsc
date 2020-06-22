90c8d7d23505285c3c1fe2e269b0cbfbee60c9d4
Robinson Espejo <robinson.espejo.1998@gmail.com>
2020-06-21 22:05:16 -0500

    UPDATE

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
   
   <!--*******************************************************************************************-->
   <!--Carrusel-->
-
+  <center>
   <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
@@ -72,15 +72,16 @@
           </div>
         </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <a class="carousel-control-prev w-75" href="#carouselExampleCaptions" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <a class="carousel-control-next w-75" href="#carouselExampleCaptions" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
   </div>
+</center>
   <!--*******************************************************************************************-->
   <!-- -->
   <div class="row">
