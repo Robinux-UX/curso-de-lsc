a0a40a66bcf664be360c7c439c17e5c88351d3f3
Robinson Espejo <robinson.espejo.1998@gmail.com>
2020-06-21 19:07:50 -0500

    UPDATE

--- curso.php
+++ curso.php
@@ -42,150 +42,124 @@
         </ul>
       </div>
 
-  <ul class="nav nav-tags" id="myTab" role="tablist">
+  <ul class="nav nav-pills" id="myTab" role="tablist">
     <li class="nav-item">
-        <a class="nav-link nav-btn-primary active" width="85%" id="l1-tab" data-toggle="tab" href="#l1" role="tab" aria-controls="l1" aria-selected="true">Leccion 1</a>
+        <a class="nav-link active" width="85%" id="l1-tab" data-toggle="pill" href="#l1" role="tab" aria-controls="l1" aria-selected="true">Leccion 1</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link nav-btn-primary" width="85%" id="l2-tab" data-toggle="tab" href="#l2" role="tab" aria-controls="l2" aria-selected="false">Leccion 2</a>
+        <a class="nav-link" width="85%" id="l2-tab" data-toggle="pill" href="#l2" role="tab" aria-controls="l2" aria-selected="false">Leccion 2</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link nav-btn-primary" width="85%" id="l3-tab" data-toggle="tab" href="#l3" role="tab" aria-controls="l3" aria-selected="false">Leccion 3</a>
+        <a class="nav-link" width="85%" id="l3-tab" data-toggle="pill" href="#l3" role="tab" aria-controls="l3" aria-selected="false">Leccion 3</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link nav-btn-primary" width="85%" id="l4-tab" data-toggle="tab" href="#l4" role="tab" aria-controls="l4" aria-selected="false">Leccion 4</a>
+        <a class="nav-link" width="85%" id="l4-tab" data-toggle="pill" href="#l4" role="tab" aria-controls="l4" aria-selected="false">Leccion 4</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link nav-btn-primary" width="85%" id="l5-tab" data-toggle="tab" href="#l5" role="tab" aria-controls="l5" aria-selected="false">Leccion 5</a>
+        <a class="nav-link" width="85%" id="l5-tab" data-toggle="pill" href="#l5" role="tab" aria-controls="l5" aria-selected="false">Leccion 5</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link nav-btn-primary" width="85%" id="l6-tab" data-toggle="tab" href="#l6" role="tab" aria-controls="l6" aria-selected="false">Leccion 6</a>
+        <a class="nav-link" width="85%" id="l6-tab" data-toggle="pill" href="#l6" role="tab" aria-controls="l6" aria-selected="false">Leccion 6</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link nav-btn-primary" width="85%" id="l7-tab" data-toggle="tab" href="#l7" role="tab" aria-controls="l7" aria-selected="false">Leccion 7</a>
+        <a class="nav-link" width="85%" id="l7-tab" data-toggle="pill" href="#l7" role="tab" aria-controls="l7" aria-selected="false">Leccion 7</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link nav-btn-primary" width="85%" id="l8-tab" data-toggle="tab" href="#l8" role="tab" aria-controls="l8" aria-selected="false">Leccion 8</a>
+        <a class="nav-link" width="85%" id="l8-tab" data-toggle="pill" href="#l8" role="tab" aria-controls="l8" aria-selected="false">Leccion 8</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link nav-btn-primary" width="85%" id="l9-tab" data-toggle="tab" href="#l9" role="tab" aria-controls="l9" aria-selected="false">Leccion 9</a>
+        <a class="nav-link" width="85%" id="l9-tab" data-toggle="pill" href="#l9" role="tab" aria-controls="l9" aria-selected="false">Leccion 9</a>
     </li>
   </ul>
 
 
 <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show" id="l1" role="tabpanel" aria-labelledby="l1-tab">
+
+    
         <!--video 1 -->
-        <div class="d-flex justify-content-center">
+        <div class="tab-pane fade show" id="l1" role="tabpanel" aria-labelledby="l1-tab">
+                <h5>Leccion 1: Abecedario</h5>
             <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" width="560px" height="315px" src="https://www.youtube.com/embed/JMraBJsA9oI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="card-body">
-                <h5 class="card-text">Leccion 1: Abecedario</h5>
-            </div>
-        </div>
         </div>
     </div>
         <!--Fin-->
 
-    <div class="tab-pane fade show" id="l2" role="tabpanel" aria-labelledby="l2-tab">
+
         <!-- video 2 -->
-        <div class="d-flex justify-content-center">
+    <div class="tab-pane fade show" id="l2" role="tabpanel" aria-labelledby="l2-tab">
+                <h5>Leccion 2: Verbos</h5>
             <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" width="560px" height="315px" src="https://www.youtube.com/embed/t2GrmD2Lhe0?start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="card-body">
-                <h5 class="card-text">Leccion 2: Verbos</h5>
-            </div>
-        </div>
         </div>
     </div>
         <!--Fin-->
 
-    <div class="tab-pane fade show" id="l3" role="tabpanel" aria-labelledby="l3-tab">
+
         <!-- video 3 -->
-        <div class="d-flex justify-content-center">
+    <div class="tab-pane fade show" id="l3" role="tabpanel" aria-labelledby="l3-tab">
+                <h5>Leccion 3: Adjetivo</h5>
             <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/-D5YsNax_1I?start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="card-body">
-                <h5 class="card-text">Leccion 3: Adjetivo</h5>
-            </div>
-        </div>
         </div>
     </div>
         <!--Fin-->
 
-    <div class="tab-pane fade show" id="l4" role="tabpanel" aria-labelledby="l4-tab">
+
         <!-- video 4 -->
-        <div class="d-flex justify-content-center">
+    <div class="tab-pane fade show" id="l4" role="tabpanel" aria-labelledby="l4-tab">
+                <h5>Leccion 4: Saludos y expresiones de costería</h5>
             <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/1MB4zx7Jq-Y?start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="card-body">
-                <h5 class="card-text">Leccion 4: Saludos y expresiones de costería</h5>
-            </div>
-        </div>
         </div>
     </div>
         <!--Fin-->
 
-    <div class="tab-pane fade show" id="l5" role="tabpanel" aria-labelledby="l5-tab">
+
         <!-- video 5 -->
-        <div class="d-flex justify-content-center">
+    <div class="tab-pane fade show" id="l5" role="tabpanel" aria-labelledby="l5-tab">
+                <h5>Leccion 5: Emociones</h5>
             <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/R4AkgkXx_yg?start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="card-body">
-                <h5 class="card-text">Leccion 5: Emociones</h5>
-            </div>
-        </div>
         </div>
     </div>
         <!--Fin-->
 
-    <div class="tab-pane fade show" id="l6" role="tabpanel" aria-labelledby="l6-tab">
+
         <!-- video 6 -->
-        <div class="d-flex justify-content-center">
+    <div class="tab-pane fade show" id="l6" role="tabpanel" aria-labelledby="l6-tab">
+                <h5>Leccion 6: Pronombres</h5>
             <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/bDjqgzW8_4w?start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="card-body">
-                <h5 class="card-text">Leccion 6: Pronombres</h5>
-            </div>
-        </div>
         </div>
     </div>
         <!--Fin-->
 
-    <div class="tab-pane fade show" id="l7" role="tabpanel" aria-labelledby="l7-tab">
+
         <!-- video 7 -->
-        <div class="d-flex justify-content-center">
+    <div class="tab-pane fade show" id="l7" role="tabpanel" aria-labelledby="l7-tab">
+                <h5>Leccion 7: Familia</h5>
             <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/wheNDU97YcM?start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="card-body">
-                <h5 class="card-text">Leccion 7: Familia</h5>
-            </div>
-        </div>
         </div>
     </div>
         <!--Fin-->
 
-    <div class="tab-pane fade show" id="l8" role="tabpanel" aria-labelledby="l8-tab">
+
         <!-- video 8 -->
-        <div class="d-flex justify-content-center">
+    <div class="tab-pane fade show" id="l8" role="tabpanel" aria-labelledby="l8-tab">
+                <h5>Leccion 8: Pronombres interrogativos</h5>
             <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/1Q7CBRz7cvY?start=10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="card-body">
-                <h5 class="card-text">Leccion 8: Pronombres interrogativos</h5>
-            </div>
-        </div>
         </div>
     </div>
         <!--Fin-->
-    <div class="tab-pane fade show" id="l9" role="tabpanel" aria-labelledby="l9-tab">        
+
         <!-- video final -->
-        <div class="d-flex justify-content-center">
+    <div class="tab-pane fade show" id="l9" role="tabpanel" aria-labelledby="l9-tab">        
+                <h5>Leccion final: Vocabulario</h5>
             <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/6woXLMPUc-w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <div class="card-body">
-                <h5 class="card-text">Leccion final: Vocabulario</h5>
-            </div>
-        </div>
         </div>
     </div>
 </div>
