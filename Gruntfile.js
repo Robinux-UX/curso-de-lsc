8690738c7ffc2bff7dab06129ac28185f5b3b11f
Robinson Espejo <robinson.espejo.1998@gmail.com>
2020-06-22 20:09:18 -0500

    UPDATE

---
+++ Gruntfile.js
@@ -0,0 +1,157 @@
+module.exports = function(grunt){
+	require('time-grunt')(grunt);
+	require('jit-grunt')(grunt, {
+		useminPrepare: 'grunt-usemin'
+	})
+	grunt.initConfig({
+		sass: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'css',
+					src: ['*.scss'],
+					dest: 'css',
+					exit '.css'
+				}]
+			}
+		},
+		watch: {
+			files: ['css/*.scss'],
+			tasks: ['css']
+		},
+
+		browserSync: {
+			dev: {
+				bdFiles: {
+					src: [
+						'css/*.css',
+						'*.html',
+						'js/*.js'
+					]
+				},
+				options: {
+					watchTask: true,
+					server: {
+						baseDir: './'
+					}
+				}
+			}
+		},
+		imagemin:{
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: './',
+					src: 'imges/*.{png, gif, jpg, jpeg}',
+					dest: 'dist/'
+				}]
+			}
+		},
+
+		copy: {
+			html: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: './',
+					src: ['*.html'],
+					dest: 'dist'
+				}]
+			},
+			font: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: 'node_modules/open-iconic/font',
+					src: ['fonts/*.*'],
+					dest: 'dist'
+				}]
+			}
+		},
+
+		clean: {
+			build: {
+				src: ['dist/']
+			}
+		},
+
+		cssmin: {
+			dist: {}
+		},
+
+		filerev: {
+			encoding: 'utf8',
+			algorithm: 'md5',
+			length: 20
+		},
+
+		release: {
+			//filerev: release hashes(md5) all assets (images, js and css)
+			//in dist directory
+			files: [{
+				src: [
+					'dist/js/*.js',
+					'dist/css/*.css'
+				]
+			}]
+		},
+
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {}
+		},
+
+		useminPrepare: {
+			foo: {
+				dest: 'dist',
+				src: ['index.html', 'header.html', 'footer.html', 'contacto.html', 'curso.html']
+			},
+			options: {
+				steps: {
+					css: ['cssmin'],
+					js: ['uglify']
+				},
+				post: {
+					css: [{
+						name: 'cssmin',
+						createConfig: function(context, block){
+							var generated = context.options.generated;
+							generated.options = {
+								keepSpecialComents: 0,
+								rebase: false
+							}
+						}
+					}]
+				}
+			}
+		},
+
+		usemin: {
+			html: ['dist/index.html', 'dist/header.html', 'dist/footer.html', 'dist/curso.html', 'dist/contacto.html'],
+			options: {
+				assetsDir: ['dist', 'dist/css', 'dist/js']
+			}
+		}
+	});
+
+	/*grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');*/
+	grunt.registerTask('css', ['sass']);
+	grunt.registerTask('default', ['browserSync', 'watch']);
+	grunt.registerTask('img:compress', ['imagemin']);
+	grunt.registerTask('build', [
+		'clean',
+		'copy',
+		'imagemin',
+		'useminPrepare',
+		'concat',
+		'cssmin',
+		'uglify',
+		'filerev',
+		'usemin'
+		])
+};
